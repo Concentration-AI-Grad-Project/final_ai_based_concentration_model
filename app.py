c02f72702b7d58533a7ea74c91554142101fa855
julia-kimm <kyujeong1387@gmail.com>
2026-05-20 15:27:50 +0900

    add user 8 and 9

--- app.py
+++ app.py
@@ -370,7 +370,7 @@ def handle_collect_sample(data):
             return
 
 
-        csv_path = os.path.join(DATASET_DIR, "collect_user_7.csv")
+        csv_path = os.path.join(DATASET_DIR, "collect_user_8.csv")
         is_new   = not os.path.exists(csv_path)
         
         with open(csv_path, "a", newline="", encoding="utf-8") as f:
